8c639dd0b606e0c5446059710a45ab2d65d494f2
cornelka <40665875+cornelka@users.noreply.github.com>
2021-06-02 15:37:22 +0200

    Add files via upload

---
+++ app/base/forms.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from flask_wtf import FlaskForm
+from wtforms import TextField, PasswordField, FileField
+from wtforms.validators import InputRequired, Email, DataRequired
+
+## login and registration
+
+class LoginForm(FlaskForm):
+    username = TextField    ('Username', id='username_login'   , validators=[DataRequired()])
+    password = PasswordField('Password', id='pwd_login'        , validators=[DataRequired()])
+
+class CreateAccountForm(FlaskForm):
+    username = TextField('Username'     , id='username_create' , validators=[DataRequired()])
+    email    = TextField('Email'        , id='email_create'    , validators=[DataRequired(), Email()])
+    password = PasswordField('Password' , id='pwd_create'      , validators=[DataRequired()])
+
+class AIcookForm(FlaskForm):
+    detect_image = FileField ('Photo', id='detectimage_photo') #, validators=[Regexp(u'^[^/\\]\.jpg$')])
+    pair_ingredients = TextField    ('Ingredients', id='ingredients'   , validators=[])
+    present_menu = TextField    ('Menu', id='menu'   , validators=[])
