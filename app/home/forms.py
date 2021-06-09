ac8fcdd904091cf06dbe00766214552a8e982c12
cornelka <40665875+cornelka@users.noreply.github.com>
2021-06-09 18:40:10 +0200

    Add files via upload

--- app/home/forms.py
+++ app/home/forms.py
@@ -12,4 +12,4 @@ from wtforms.validators import InputRequired, Email, DataRequired
 class AIcookForm(FlaskForm):
     detect_image = FileField ('Photo', id='detect_image') #, validators=[Regexp(u'^[^/\\]\.jpg$')])
     pair_ingredients = TextField    ('Ingredients', id='ingredients'   , validators=[])
-    present_menu = TextField    ('Menu', id='menu'   , validators=[])
+    present_wine = TextField    ('Wine', id='wine'   , validators=[])
--- app/home/routes.py
+++ app/home/routes.py
@@ -9,7 +9,6 @@ from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
 
-
 ###################################################################################################
 from app.home.backend.pair.pairing import getRecipeAndWine
 from app.home.backend.present.presenting import getSommelier
@@ -21,75 +20,151 @@ from app.home.forms import AIcookForm
 import pandas as pd
 
 
-# aicook
-# import sys
-# sys.path.insert(0, './yolov5')
-# #sys.path.insert(0,'/cook/backend/detect/yolov5')
-# #sys.path.insert(0,'\AIcook\backend\detect\yolov5')
-# import backend.detect.yolov5.detect_aicook as detect_aicook
-
+from imgaug import augmenters as iaa
 import torch
 import cv2
-y5 = torch.hub.load('ultralytics/yolov5', 'custom', path='app/home/backend/detect/yolov5/aicook.pt',force_reload=True)
+from werkzeug.utils import secure_filename
 
+y5 = torch.hub.load('ultralytics/yolov5', 'custom', path='app/home/backend/detect/yolov5/aicook.pt',force_reload=True)
 y5.conf = 0.6
 
-
-
-from werkzeug.utils import secure_filename
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 #some globals to use in the various front-end sections
-pairing_txt = ''
-presenting_txt = ''
+recipe_display = ''
+pairing_display = ''
+presenting_display = ''
 filename = ''
-top_ingredients_txt = [pd.DataFrame().to_html(index=False, classes='table tablesorter')]
+top_ingredients_display = ''
+
+def resize(img):
+    #resize the image to a square + also scale the bounding boxes
+    #we assume that the image is vertically oriented and fill the edge with black pixels
+
+    SIZE= 640    
+    prepro = iaa.Sequential([
+        iaa.Resize({"height": SIZE, "width": "keep-aspect-ratio"}),
+        iaa.CenterPadToFixedSize(height=SIZE, width=SIZE)    
+
+    ], random_order=False)
+
+    return prepro(image=img)
 
 
 def doDetection(inputfile):
-    top = 50
+    
     filename = secure_filename(inputfile.filename)
-    base_img = os.path.join('app/base/static/uploads/', filename)
-    inputfile.save(base_img)
-                
-    # Inference
-    y5Result = y5(base_img)
+    base_img_path = os.path.join('app/base/static/uploads/', filename)
+    inputfile.save(base_img_path)
+    
+    # resize the original image
+    base_img = cv2.imread(base_img_path)    
+    scaled_img = resize(base_img)        
+    
+    scaled_img_path = os.path.join('app/base/static/uploads/', 'scaled_' + filename)    
+    cv2.imwrite(scaled_img_path, scaled_img)    
+    
+    # Inference using YOLOv5 with own aicook weigths
+    y5Result = y5(scaled_img_path)
     
     #render boxes on image
     y5Result.render() 
+        
+    #change image color for correct visualisation
+    target_img = cv2.cvtColor( y5Result.imgs[0], cv2.COLOR_BGR2RGB)
     
-    target_img =   cv2.rotate(cv2.cvtColor( y5Result.imgs[0], cv2.COLOR_BGR2RGB), cv2.ROTATE_90_COUNTERCLOCKWISE)
+    #save resulting image (still scaled) with bounding boxes
+    cv2.imwrite(base_img_path.replace('/uploads/','/uploads/exp/'),target_img)    
     
-    cv2.imwrite(base_img.replace('/uploads/','/uploads/exp/'),target_img)    
+    #get the detected labels(ingredients)
+    if y5Result.pandas().xyxy[0].empty:
+        flash('No ingredients found in this image','detection_result')     
+        return filename, '', None
+    else:
+        top_ingredient_names = y5Result.pandas().xyxy[0].loc[:, ['name']]
+        top_ingredient_names.rename(columns={'name': 'food'}, inplace=True) #pairing expects a food column
     
-    top_ingredient_names = y5Result.pandas().xyxy[0].loc[:, ['name']].to_string()           
-    top_ingr = y5Result.pandas().xyxy[0].loc[:top-1, ['confidence','name']]
-    top_ingredients_display = [top_ingr.to_html(index=False, classes='table tablesorter')]    
+        # visualize the top ingredients with their confidence scores
+        top = 50    
+        top_ingr = y5Result.pandas().xyxy[0].loc[:top-1, ['confidence','name']]    
+        top_ingredients_disp = [top_ingr.to_html(index=False, classes='table tablesorter')]    
 
-    return filename, top_ingredients_display, top_ingredient_names
+        return filename, top_ingredients_disp, top_ingredient_names
     
     
 def doPairing(ingr):
-    if ingr:
-        return getRecipeAndWine(ingr)        
-    else:
-        flash('No ingredients supplied','pair')
-    
+    if ingr is None:
+        flash('No ingredients supplied','pair')      
+        return None,'',''
         
-def dopresenting(menu):
-    if menu:
-        return getSommelier(menu)
-    else:
-        flash('No menu supplied', 'present')
+    elif ingr.empty:
+        flash('No ingredients supplied','pair')
+        return None,'',''
     
+    elif ingr.dropna().empty:
+        flash('No ingredients supplied','pair')     
+        return None,'',''
+    
+    elif len(ingr['food']) == 0:
+        flash('No ingredients supplied','pair')        
+        return None,'',''
+    
+    else:
+        dfRecipe, dfWine = getRecipeAndWine(ingr)   
+        
+        if dfRecipe is not None:        
+            
+            dfRecipe['top_ingredients'] = dfRecipe['ingredient_top_match'].apply(lambda ttl: ', '.join(ttl))
+            dfRecipe.rename(columns={'title': 'recipe'}, inplace=True)
+            recipe_display = [dfRecipe.loc[:,['recipe','top_ingredients']].to_html(index=False, classes='table tablesorter')]   
+        else:
+            recipe_display = [pd.DataFrame(columns=['recipe'], data=[['No recipe found']]).to_html(index=False, classes='table tablesorter')]   
+        
 
+        if dfWine is not None:
+            dfWine.rename(columns={'title': 'wine'}, inplace=True)            
+            pair_display =  [dfWine.loc[:,['wine','country', 'winery', 'variety', 'description']].to_html(index=False, classes='table tablesorter')]   
+
+        else:
+            pair_display = [pd.DataFrame(columns=['wine'], data=[['No wine found']]).to_html(index=False, classes='table tablesorter')]   
+            
+        
+        return dfWine, pair_display, recipe_display    
+        
+        
+def dopresenting(wine, context):   
+
+    if wine is None:
+        flash('No wine info supplied', 'present')
+        return ''
+        
+    elif wine.empty:
+        flash('No wine info supplied', 'present')
+        return ''
+    
+    elif wine.dropna(how='all').empty:
+        flash('No wine info supplied', 'present')
+        return ''
+    
+    elif len(wine['description']) == 0:
+        flash('No wine info supplied', 'present')
+        return ''
+
+    elif wine['description'].values.size == 0:
+        flash('No wine info supplied', 'present')
+        return ''
+        
+    else:
+        presentation = getSommelier(wine, context)
+
+        return presentation
+        
 
 @blueprint.route('/cook', methods=['GET', 'POST'])
 def cook():
-    global pairing_txt, presenting_txt, filename, top_ingredients_txt
+    global pairing_display, presenting_display, filename, top_ingredients_display,recipe_display
         
     aicookF = AIcookForm(request.form)
     
@@ -108,14 +183,15 @@ def cook():
         
         if file and allowed_file(file.filename):
 
-            filename, top_ingredients_txt, _ = doDetection(file)            
+            filename, top_ingredients_display, _ = doDetection(file)            
 
             return render_template('cook.html', 
                                    form=aicookF, 
                                    filename= filename, 
-                                   top_ingredients= top_ingredients_txt,
-                                   pairing = pairing_txt,
-                                   presenting = presenting_txt
+                                   top_ingredients= top_ingredients_display,
+                                   pairing = pairing_display,
+                                   recipe = recipe_display,
+                                   presenting = presenting_display
                                    )
                                    
         else:
@@ -136,18 +212,19 @@ def cook():
         
         if file and allowed_file(file.filename):
 
-            filename, top_ingredients_txt, ingr_names = doDetection(file)            
+            filename, top_ingredients_display, ingr_names = doDetection(file)                    
+        
+            pairing, pairing_display,recipe_display = doPairing(ingr_names)                    
             
-            pairing_txt = doPairing(ingr_names)        
-            
-            presenting_txt = dopresenting(pairing_txt)
+            presenting_display = dopresenting(pairing,True)
             
             return render_template('cook.html', 
                                    form=aicookF, 
                                    filename= filename, 
-                                   top_ingredients= top_ingredients_txt,
-                                   pairing = pairing_txt,
-                                   presenting = presenting_txt
+                                   top_ingredients= top_ingredients_display,
+                                   pairing = pairing_display,
+                                   recipe = recipe_display,                                   
+                                   presenting = presenting_display
                                    )
                                    
         else:
@@ -159,48 +236,64 @@ def cook():
         return render_template('cook.html', 
                                form=aicookF, 
                                filename=filename, 
-                               top_ingredients= top_ingredients_txt,
-                               pairing = pairing_txt,
-                               presenting = presenting_txt
+                               top_ingredients= top_ingredients_display,
+                               pairing = pairing_display,
+                               recipe = recipe_display,                               
+                               presenting = presenting_display
                                )
 
 
     elif 'btn_pair' in request.form:        
+        if request.form['pair_ingredients'] == '':
+            ingr_input = None 
+        else:
+            ingr_input = pd.DataFrame(request.form['pair_ingredients'].split(','),columns=['food'] )
 
-        pairing_txt = doPairing(request.form['pair_ingredients'])
+        _, pairing_display, recipe_display = doPairing(ingr_input)
 
         return render_template('cook.html', 
                                form=aicookF, 
                                filename=filename, 
-                               top_ingredients= top_ingredients_txt,
-                               pairing = pairing_txt,
-                               presenting = presenting_txt
+                               top_ingredients= top_ingredients_display,
+                               pairing = pairing_display,
+                               recipe = recipe_display,                               
+                               presenting = presenting_display
                                )
 
     elif 'btn_present' in request.form:    
-        presenting_txt = dopresenting(request.form['present_menu'])
+        if request.form['present_wine'] == '':
+            present_input = None
+        else:
+            present_input = pd.DataFrame( [request.form['present_wine']] ,columns=['description'] )                
+            
+        presenting_display = dopresenting(present_input, False)
 
         return render_template('cook.html', 
                                form=aicookF, 
                                filename=filename, 
-                               top_ingredients= top_ingredients_txt,
-                               pairing = pairing_txt,
-                               presenting = presenting_txt
+                               top_ingredients= top_ingredients_display,
+                               pairing = pairing_display,
+                               recipe = recipe_display,                               
+                               presenting = presenting_display
                                )
 
     elif 'btn_reset' in request.form:
         #reset all global variables
-        pairing_txt = ''
-        presenting_txt = ''
+        pairing_display = ''        
+        recipe_display = ''
+        presenting_display = ''
         filename = ''
-        top_ingredients_txt = [pd.DataFrame().to_html(index=False, classes='table tablesorter')]
-        
+        top_ingredients_display = ''
+        aicookF.pair_ingredients.data = ''
+        aicookF.present_wine.data = ''
+                
         return render_template('cook.html', 
                                form=aicookF, 
                                filename=filename, 
-                               top_ingredients= top_ingredients_txt,
-                               pairing = pairing_txt,
-                               presenting = presenting_txt
+                               top_ingredients= top_ingredients_display,
+                               pairing = pairing_display,
+                               recipe = recipe_display,                               
+                               presenting = presenting_display
                                )
     
     return render_template('cook.html', form=aicookF, filename= '')
@@ -208,16 +301,12 @@ def cook():
 
 @blueprint.route('/uploads/<filename>')
 def display_orig_image(filename):
-    #flash('display_image filename: ' + filename)
     return redirect(url_for('static', filename= 'uploads/' +filename), code=301)
-    #return redirect(request.url)  
 
 
 @blueprint.route('/uploads/exp/<filename>')
 def display_detection_image(filename):
-    #flash('display_image filename: ' + filename)
     return redirect(url_for('static', filename= 'uploads/exp/' +filename), code=301)
-    #return redirect(request.url)  
     
 ###################################################################################################
 
